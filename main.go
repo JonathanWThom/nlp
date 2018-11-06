c367950b39f910bc29331147ab5c59019777834d
Jonathan Thom <jonathan.thom1990@gmail.com>
2018-11-05 23:38:34 -0800

    Takes command line flag and writes to stdout

--- main.go
+++ main.go
@@ -3,39 +3,49 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	language "cloud.google.com/go/language/apiv1"
 	languagepb "google.golang.org/genproto/googleapis/cloud/language/v1"
 )
 
 func main() {
+	var text, output string
+	flag.StringVar(&text, "t", "", "Text to be analyzed")
+	// 	flag.String(output, "o", "File to write to, defaults to stdout")
+	flag.Parse()
+
+	if text == "" {
+		log.Fatalf("Must enter text with flag -t [your_text_here]")
+	}
+
+	w := os.Stdout
+	//	var file *os.File
+
+	if output != "" {
+		file, err := os.OpenFile("output.txt", os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			log.Fatalf("File cannot be created")
+		}
+		defer file.Close()
+		// 	w = bufio.NewWriter(file)
+	}
+
 	ctx := context.Background()
 	client, err := language.NewClient(ctx)
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
 	}
 
-	text := `
-		He was an old man who fished alone in a skiff in the Gulf Stream and he had
-		gone eighty-four days now without taking a fish. In the first forty days a
-		boy had been with him. But after forty days without a fish the boy's parents
-		had told him that the old man was now definitely and finally salao, which is
-		the worst form of unlucky, and the boy had gone at their orders in another
-		boat which caught three good fish the first week. It made the boy sad to see
-		the old man come in each day with his skiff empty and he always went down to
-		help him carry either the coiled lines or the gaff and harpoon and the sail
-		that was furled around the mast. The sail was patched with flour sacks and,
-		furled, it looked like the flag of permanent defeat.
-	`
-
 	syntax, err := analyzeSyntax(ctx, client, text)
 	if err != nil {
 		log.Fatalf("Failed to analyze text: %v", err)
 	}
 
-	fmt.Printf("TEXT:\n%s\n\n", text)
+	fmt.Fprintf(w, "TEXT:\n%s\n\n", text)
 
 	index := 0
 
@@ -45,26 +55,26 @@ func main() {
 		content := text.Content
 		sentenceBegin := int(text.BeginOffset)
 		sentenceEnd := sentenceBegin + len(content) - 1
-		fmt.Printf("SENTENCE: %s\n\n", content)
+		fmt.Fprintf(w, "SENTENCE: %s\n\n", content)
 
 		for index < len(syntax.Tokens) && int(syntax.Tokens[index].Text.BeginOffset) <= sentenceEnd {
 			token := syntax.Tokens[index]
-			fmt.Printf("Content: %s\n", token.Text.Content)
-			fmt.Printf("Part of Speech (Tag): %s\n", token.PartOfSpeech.Tag)
-			fmt.Printf("Part of Speech (Number): %s\n", token.PartOfSpeech.Number)
-			fmt.Printf("Part of Speech (Person): %s\n", token.PartOfSpeech.Person)
-			fmt.Printf("Part of Speech (Gender): %s\n", token.PartOfSpeech.Gender)
-			fmt.Printf("Part of Speech (Case): %s\n", token.PartOfSpeech.Case)
-			fmt.Printf("Part of Speech (Tense): %s\n", token.PartOfSpeech.Tense)
-			fmt.Printf("Part of Speech (Aspect): %s\n", token.PartOfSpeech.Aspect)
-			fmt.Printf("Part of Speech (Mood): %s\n", token.PartOfSpeech.Mood)
-			fmt.Printf("Part of Speech (Voice): %s\n", token.PartOfSpeech.Voice)
-			fmt.Printf("Part of Speech (Reciprocity): %s\n", token.PartOfSpeech.Reciprocity)
-			fmt.Printf("Part of Speech (Proper): %s\n", token.PartOfSpeech.Proper)
-			fmt.Printf("Part of Speech (Form): %s\n", token.PartOfSpeech.Form)
-			fmt.Printf("Dependency Edge (Head Token Index): %d\n", token.DependencyEdge.HeadTokenIndex)
-			fmt.Printf("Dependency Edge (Label): %s\n", token.DependencyEdge.Label)
-			fmt.Printf("Lemma: %s\n\n", token.Lemma)
+			fmt.Fprintf(w, "Content: %s\n", token.Text.Content)
+			fmt.Fprintf(w, "Part of Speech (Tag): %s\n", token.PartOfSpeech.Tag)
+			fmt.Fprintf(w, "Part of Speech (Number): %s\n", token.PartOfSpeech.Number)
+			fmt.Fprintf(w, "Part of Speech (Person): %s\n", token.PartOfSpeech.Person)
+			fmt.Fprintf(w, "Part of Speech (Gender): %s\n", token.PartOfSpeech.Gender)
+			fmt.Fprintf(w, "Part of Speech (Case): %s\n", token.PartOfSpeech.Case)
+			fmt.Fprintf(w, "Part of Speech (Tense): %s\n", token.PartOfSpeech.Tense)
+			fmt.Fprintf(w, "Part of Speech (Aspect): %s\n", token.PartOfSpeech.Aspect)
+			fmt.Fprintf(w, "Part of Speech (Mood): %s\n", token.PartOfSpeech.Mood)
+			fmt.Fprintf(w, "Part of Speech (Voice): %s\n", token.PartOfSpeech.Voice)
+			fmt.Fprintf(w, "Part of Speech (Reciprocity): %s\n", token.PartOfSpeech.Reciprocity)
+			fmt.Fprintf(w, "Part of Speech (Proper): %s\n", token.PartOfSpeech.Proper)
+			fmt.Fprintf(w, "Part of Speech (Form): %s\n", token.PartOfSpeech.Form)
+			fmt.Fprintf(w, "Dependency Edge (Head Token Index): %d\n", token.DependencyEdge.HeadTokenIndex)
+			fmt.Fprintf(w, "Dependency Edge (Label): %s\n", token.DependencyEdge.Label)
+			fmt.Fprintf(w, "Lemma: %s\n\n", token.Lemma)
 
 			index++
 		}
