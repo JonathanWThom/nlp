e867957d936be6e08c5b2dcf725dbd8e547c2080
Jonathan Thom <jonathan.thom1990@gmail.com>
2018-11-05 22:47:13 -0800

    Returns tokens unique to each sentence

--- main.go
+++ main.go
@@ -17,30 +17,43 @@ func main() {
 		log.Fatalf("Failed to create client: %v", err)
 	}
 
-	syntax, err := analyzeSyntax(ctx, client, "The quick brown fox jumped over the lazy dog.")
+	syntax, err := analyzeSyntax(ctx, client, "The quick brown fox jumped over the lazy dog. dog dog.")
 	if err != nil {
 		log.Fatalf("Failed to analyze text: %v", err)
 	}
 
-	fmt.Println(syntax.Sentences[0].Text.Content)
-	for i := 0; i < len(syntax.Tokens); i++ {
-		token := syntax.Tokens[i]
-		fmt.Printf("Content: %s\n", token.Text.Content)
-		fmt.Printf("Part of Speech (Tag): %s\n", token.PartOfSpeech.Tag)
-		fmt.Printf("Part of Speech (Number): %s\n", token.PartOfSpeech.Number)
-		fmt.Printf("Part of Speech (Person): %s\n", token.PartOfSpeech.Person)
-		fmt.Printf("Part of Speech (Gender): %s\n", token.PartOfSpeech.Gender)
-		fmt.Printf("Part of Speech (Case): %s\n", token.PartOfSpeech.Case)
-		fmt.Printf("Part of Speech (Tense): %s\n", token.PartOfSpeech.Tense)
-		fmt.Printf("Part of Speech (Aspect): %s\n", token.PartOfSpeech.Aspect)
-		fmt.Printf("Part of Speech (Mood): %s\n", token.PartOfSpeech.Mood)
-		fmt.Printf("Part of Speech (Voice): %s\n", token.PartOfSpeech.Voice)
-		fmt.Printf("Part of Speech (Reciprocity): %s\n", token.PartOfSpeech.Reciprocity)
-		fmt.Printf("Part of Speech (Proper): %s\n", token.PartOfSpeech.Proper)
-		fmt.Printf("Part of Speech (Form): %s\n", token.PartOfSpeech.Form)
-		fmt.Printf("Dependency Edge (Head Token Index): %d\n", token.DependencyEdge.HeadTokenIndex)
-		fmt.Printf("Dependency Edge (Label): %s\n", token.DependencyEdge.Label)
-		fmt.Printf("Lemma: %s\n\n\n", token.Lemma)
+	index := 0
+
+	for i := 0; i < len(syntax.Sentences); i++ {
+		sentence := syntax.Sentences[i]
+		text := sentence.Text
+		content := text.Content
+		sentenceBegin := int(text.BeginOffset)
+		sentenceEnd := sentenceBegin + len(content) - 1
+		fmt.Println(sentenceEnd)
+		fmt.Printf("SENTENCE: %s\n\n", content)
+
+		for index < len(syntax.Tokens) && int(syntax.Tokens[index].Text.BeginOffset) <= sentenceEnd {
+			token := syntax.Tokens[index]
+			fmt.Printf("Content: %s\n", token.Text.Content)
+			fmt.Printf("Part of Speech (Tag): %s\n", token.PartOfSpeech.Tag)
+			fmt.Printf("Part of Speech (Number): %s\n", token.PartOfSpeech.Number)
+			fmt.Printf("Part of Speech (Person): %s\n", token.PartOfSpeech.Person)
+			fmt.Printf("Part of Speech (Gender): %s\n", token.PartOfSpeech.Gender)
+			fmt.Printf("Part of Speech (Case): %s\n", token.PartOfSpeech.Case)
+			fmt.Printf("Part of Speech (Tense): %s\n", token.PartOfSpeech.Tense)
+			fmt.Printf("Part of Speech (Aspect): %s\n", token.PartOfSpeech.Aspect)
+			fmt.Printf("Part of Speech (Mood): %s\n", token.PartOfSpeech.Mood)
+			fmt.Printf("Part of Speech (Voice): %s\n", token.PartOfSpeech.Voice)
+			fmt.Printf("Part of Speech (Reciprocity): %s\n", token.PartOfSpeech.Reciprocity)
+			fmt.Printf("Part of Speech (Proper): %s\n", token.PartOfSpeech.Proper)
+			fmt.Printf("Part of Speech (Form): %s\n", token.PartOfSpeech.Form)
+			fmt.Printf("Dependency Edge (Head Token Index): %d\n", token.DependencyEdge.HeadTokenIndex)
+			fmt.Printf("Dependency Edge (Label): %s\n", token.DependencyEdge.Label)
+			fmt.Printf("Lemma: %s\n\n", token.Lemma)
+
+			index++
+		}
 	}
 }
 
