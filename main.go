b4bb4454d9ab29c14588739d09186d013c84060b
Jonathan Thom <jonathan.thom1990@gmail.com>
2018-11-19 22:13:10 -0800

    Uses cli package for flag management

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ package main
 import (
 	"bufio"
 	"context"
-	"flag"
 	"fmt"
+	"github.com/urfave/cli"
 	"log"
 	"os"
 
@@ -15,73 +15,96 @@ import (
 
 func main() {
 	var text, output string
-	flag.StringVar(&text, "t", "", "Text to be analyzed")
-	flag.StringVar(&output, "o", "", "File to write to, defaults to stdout")
-	flag.Parse()
-
-	if text == "" {
-		log.Fatalf("Must enter text with flag -t [your_text_here]")
+	app := cli.NewApp()
+	app.Name = "nlp"
+	app.Usage = "Parse language using Google's Natural Language Processing API"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "t",
+			Value:       "",
+			Usage:       "Text to be analyzed",
+			Destination: &text,
+		},
+		cli.StringFlag{
+			Name:        "o",
+			Value:       "",
+			Usage:       "File to write to, defaults to stdout",
+			Destination: &output,
+		},
 	}
 
-	var w *bufio.Writer
+	app.Action = func(c *cli.Context) error {
+		if text == "" {
+			log.Fatalf("Must enter text with flag -t [your_text_here]")
+		}
 
-	if output != "" {
-		file, err := os.OpenFile(output, os.O_WRONLY|os.O_CREATE, 0777)
+		var w *bufio.Writer
+
+		if output != "" {
+			file, err := os.OpenFile(output, os.O_WRONLY|os.O_CREATE, 0777)
+			if err != nil {
+				log.Fatalf("File cannot be created")
+			}
+			defer file.Close()
+			w = bufio.NewWriter(file)
+		} else {
+			w = bufio.NewWriter(os.Stdout)
+		}
+
+		defer w.Flush()
+
+		ctx := context.Background()
+		client, err := language.NewClient(ctx)
 		if err != nil {
-			log.Fatalf("File cannot be created")
+			log.Fatalf("Failed to create client: %v", err)
 		}
-		defer file.Close()
-		w = bufio.NewWriter(file)
-	} else {
-		w = bufio.NewWriter(os.Stdout)
-	}
 
-	defer w.Flush()
-
-	ctx := context.Background()
-	client, err := language.NewClient(ctx)
-	if err != nil {
-		log.Fatalf("Failed to create client: %v", err)
-	}
-
-	syntax, err := analyzeSyntax(ctx, client, text)
-	if err != nil {
-		log.Fatalf("Failed to analyze text: %v", err)
-	}
-
-	fmt.Fprintf(w, "TEXT:\n%s\n\n", text)
-
-	index := 0
-
-	for i := 0; i < len(syntax.Sentences); i++ {
-		sentence := syntax.Sentences[i]
-		text := sentence.Text
-		content := text.Content
-		sentenceBegin := int(text.BeginOffset)
-		sentenceEnd := sentenceBegin + len(content) - 1
-		fmt.Fprintf(w, "SENTENCE: %s\n\n", content)
-
-		for index < len(syntax.Tokens) && int(syntax.Tokens[index].Text.BeginOffset) <= sentenceEnd {
-			token := syntax.Tokens[index]
-			fmt.Fprintf(w, "Content: %s\n", token.Text.Content)
-			fmt.Fprintf(w, "Part of Speech (Tag): %s\n", token.PartOfSpeech.Tag)
-			fmt.Fprintf(w, "Part of Speech (Number): %s\n", token.PartOfSpeech.Number)
-			fmt.Fprintf(w, "Part of Speech (Person): %s\n", token.PartOfSpeech.Person)
-			fmt.Fprintf(w, "Part of Speech (Gender): %s\n", token.PartOfSpeech.Gender)
-			fmt.Fprintf(w, "Part of Speech (Case): %s\n", token.PartOfSpeech.Case)
-			fmt.Fprintf(w, "Part of Speech (Tense): %s\n", token.PartOfSpeech.Tense)
-			fmt.Fprintf(w, "Part of Speech (Aspect): %s\n", token.PartOfSpeech.Aspect)
-			fmt.Fprintf(w, "Part of Speech (Mood): %s\n", token.PartOfSpeech.Mood)
-			fmt.Fprintf(w, "Part of Speech (Voice): %s\n", token.PartOfSpeech.Voice)
-			fmt.Fprintf(w, "Part of Speech (Reciprocity): %s\n", token.PartOfSpeech.Reciprocity)
-			fmt.Fprintf(w, "Part of Speech (Proper): %s\n", token.PartOfSpeech.Proper)
-			fmt.Fprintf(w, "Part of Speech (Form): %s\n", token.PartOfSpeech.Form)
-			fmt.Fprintf(w, "Dependency Edge (Head Token Index): %d\n", token.DependencyEdge.HeadTokenIndex)
-			fmt.Fprintf(w, "Dependency Edge (Label): %s\n", token.DependencyEdge.Label)
-			fmt.Fprintf(w, "Lemma: %s\n\n", token.Lemma)
-
-			index++
+		syntax, err := analyzeSyntax(ctx, client, text)
+		if err != nil {
+			log.Fatalf("Failed to analyze text: %v", err)
 		}
+
+		fmt.Fprintf(w, "TEXT:\n%s\n\n", text)
+
+		index := 0
+
+		for i := 0; i < len(syntax.Sentences); i++ {
+			sentence := syntax.Sentences[i]
+			text := sentence.Text
+			content := text.Content
+			sentenceBegin := int(text.BeginOffset)
+			sentenceEnd := sentenceBegin + len(content) - 1
+			fmt.Fprintf(w, "SENTENCE: %s\n\n", content)
+
+			for index < len(syntax.Tokens) && int(syntax.Tokens[index].Text.BeginOffset) <= sentenceEnd {
+				token := syntax.Tokens[index]
+				fmt.Fprintf(w, "Content: %s\n", token.Text.Content)
+				fmt.Fprintf(w, "Part of Speech (Tag): %s\n", token.PartOfSpeech.Tag)
+				fmt.Fprintf(w, "Part of Speech (Number): %s\n", token.PartOfSpeech.Number)
+				fmt.Fprintf(w, "Part of Speech (Person): %s\n", token.PartOfSpeech.Person)
+				fmt.Fprintf(w, "Part of Speech (Gender): %s\n", token.PartOfSpeech.Gender)
+				fmt.Fprintf(w, "Part of Speech (Case): %s\n", token.PartOfSpeech.Case)
+				fmt.Fprintf(w, "Part of Speech (Tense): %s\n", token.PartOfSpeech.Tense)
+				fmt.Fprintf(w, "Part of Speech (Aspect): %s\n", token.PartOfSpeech.Aspect)
+				fmt.Fprintf(w, "Part of Speech (Mood): %s\n", token.PartOfSpeech.Mood)
+				fmt.Fprintf(w, "Part of Speech (Voice): %s\n", token.PartOfSpeech.Voice)
+				fmt.Fprintf(w, "Part of Speech (Reciprocity): %s\n", token.PartOfSpeech.Reciprocity)
+				fmt.Fprintf(w, "Part of Speech (Proper): %s\n", token.PartOfSpeech.Proper)
+				fmt.Fprintf(w, "Part of Speech (Form): %s\n", token.PartOfSpeech.Form)
+				fmt.Fprintf(w, "Dependency Edge (Head Token Index): %d\n", token.DependencyEdge.HeadTokenIndex)
+				fmt.Fprintf(w, "Dependency Edge (Label): %s\n", token.DependencyEdge.Label)
+				fmt.Fprintf(w, "Lemma: %s\n\n", token.Lemma)
+
+				index++
+			}
+		}
+
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
